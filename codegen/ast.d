7f6908f63419d87559c437b00ed6ec3bee11373f
Tasos <tasos@example.com>
2026-02-09 21:57:43 +0200

    going to restore

---
+++ codegen/ast.d
@@ -0,0 +1,6 @@
+ast.o: ast.c ast.h types.h hashtable.h symbol.h symbolTable.h
+ast.h:
+types.h:
+hashtable.h:
+symbol.h:
+symbolTable.h:
---
+++ codegen/lex.yy.d
@@ -0,0 +1,9 @@
+lex.yy.o: lex.yy.c types.h hashtable.h syntax.tab.h symbol.h ast.h \
+ astinfo.h settings.h
+types.h:
+hashtable.h:
+syntax.tab.h:
+symbol.h:
+ast.h:
+astinfo.h:
+settings.h:
